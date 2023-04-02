355af2c25d1be34dc71cf687e4d8c72def4711fb
gmahaleji <ganeshmahaaa@gmail.com>
2023-04-02 16:57:50 +0530

    login & token added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,10 @@
-const User = require("../models/userModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
 
 //Register a user
-
-exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
 
   const user = await User.create({
@@ -17,8 +17,33 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  res.status(201).json({
-    success: true,
-    user,
-  });
+  sendToken(user, 201, res);
 });
+
+//Login User
+
+const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //checking if user has given password & email both
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email & Password", 401));
+  }
+
+  const isPasswordMatched = user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email & Password", 401));
+  }
+
+  sendToken(user, 200, res);
+});
+
+module.exports = { registerUser, loginUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -33,11 +36,34 @@ const userSchema = new mongoose.Schema({
 
   role: {
     type: String,
-    default: "user",
+    // default: "user",
   },
 
   resetPasswordToken: String,
   resetPasswordExpire: Date,
 });
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password ")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+//JWT Token
+
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+//compare Password
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
